1daacbcb077a259fd45d5abb5a18605e95a49aef
alex.baiardi <alex.baiardi99@gmail.com>
2022-02-28 12:01:44 +0100

    Exercise 1 done

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,28 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList circularList;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    public void beforeEach(){
+        circularList= new CircularListImpl();
     }
+
+    @Test
+    public void testSize(){
+        assertTrue(circularList.size()==0);
+    }
+
 
 }
